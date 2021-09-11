71fd5bfeb312743e9271033acac03fce6ad794d0
Anuj <ruhela7777@gmail.com>
2021-09-11 14:43:59 +0530

    Updated Code

--- Student1.java
+++ Student1.java
@@ -1,16 +1,35 @@
-// Added constructor by Anuj Ruhela
 import java.util.Scanner;
 
 public class Student1
 {
     String name;
-    Scanner sc = new Scanner(System.in);
-    Student1()
+    float percentage;
+    Student1(String nm,int percent)
     {
-        name  =  sc.nextLine();
+        name = nm;
+        percentage = percent;
     }
-    public void show()
+}
+class Test
+{
+    public static void main(String[] args)
     {
-        System.out.println(name);
+        Student1 anuj = new Student1("Anuj", 90);
+        Student1 anurag = new Student1("Anurag", 81);
+        Student1 shivam = new Student1("Shivam", 87);
+        if (anuj.percentage> anurag.percentage)
+        {
+            if(anuj.percentage> shivam.percentage)
+                System.out.println("Name : " + anuj.name + " , Percentage : " + anuj.percentage + "%");
+            else
+                System.out.println("Name : " + shivam.name + " , Percentage : " + shivam.percentage + "%");
+        }
+        else
+        {
+            if(anurag.percentage> shivam.percentage)
+                System.out.println("Name : " + anurag.name + " , Percentage : " + anurag.percentage + "%");
+            else
+                System.out.println("Name : " + shivam.name + " , Percentage : " + shivam.percentage + "%");
+        }
     }
 }
