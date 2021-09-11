6418f79396fdedf9959f45a9dcf8d3f19e57070a
Anuj <ruhela7777@gmail.com>
2021-09-11 10:45:40 +0530

    Added Constructor for Initialization

--- Student1.java
+++ Student1.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Student1
 {
     String name;
+    Scanner sc = new Scanner(System.in);
+    Student1()
+    {
+        name  =  sc.nextLine();
+    }
+    public void show()
+    {
+        System.out.println(name);
+    }
 }
