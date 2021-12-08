e6b1c951d13888712c9dc6cda6d1957607b492d0
Anuj <ruhela7777@gmail.com>
2021-12-08 20:18:29 +0530

    Implement Functional Interface Concept

--- test.java
+++ test.java
@@ -1,17 +1,14 @@
-
 //1. Write a program to create interface named test. In this interface the member function is square.
 // Implement this interface in arithmetic class. Create one new class called ToTestInt in this class use
 // the object of arithmetic class.
-
-
 interface test
 {
     public void square();
-
 }
 
 class Arithmetic implements test
 {
+    @Override
     public void square()
     {
         System.out.println("Square");
