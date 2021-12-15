55f849adff1ea0a85c32cd3749491f3d35eb192b
Anuj <ruhela7777@gmail.com>
2021-12-15 07:47:37 +0530

    Perform various database operations by Statement Interface

---
+++ database/Day1.java
@@ -0,0 +1,28 @@
+package database;
+
+import java.sql.*;
+
+public class Day1
+{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee","root","");
+            Statement s = con.createStatement();
+            String str = "DELETE FROM emp WHERE empname='Aryan Gupta'";
+            s.executeUpdate(str);
+            ResultSet rs = s.executeQuery("SELECT * FROM emp");
+            while (rs.next())
+            {
+                System.out.println(rs.getInt(1) + "  " + rs.getString("empname"));
+            }
+            con.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
